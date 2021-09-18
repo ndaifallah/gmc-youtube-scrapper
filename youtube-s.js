613ccc0f666f7b518fd3f81ad0ecb068745163e5
elhachemi <elhachemiadmane@gmail.com>
2021-09-18 12:57:28 +0100

    modif 2

--- youtube-s.js
+++ youtube-s.js
@@ -1,3 +1,10 @@
 var b =12
 console.log("Starting");
 var c=15;
+console.log("moi elhachemi");
+var c=155;
+console.log("moi elhachemi apres checkout");
+var c=15;
+console.log("moi elhachemi");
+var c=1555;
+console.log("moi elhachemi apres checkout en double");
